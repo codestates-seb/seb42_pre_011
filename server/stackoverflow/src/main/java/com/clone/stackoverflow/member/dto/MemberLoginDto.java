90f097e48a2a980d0e78d26ae6087b0c2a39af21
nayoseb <nayoseb1018@gmail.com>
2023-02-16 01:21:56 +0900

    feat: create Member entity

---
+++ server/stackoverflow/src/main/java/com/clone/stackoverflow/member/dto/MemberLoginDto.java
@@ -0,0 +1,23 @@
+package com.clone.stackoverflow.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MemberLoginDto {
+
+    @NotBlank(message = "이메일은 공백일 수 없습니다.")
+    private String email;
+
+    @Size(min = 8, max = 20, message = "비밀번호 길이는 8 이상 20 이하여야 합니다.")
+    private String password;
+
+}
