79d335eb7050e5057172e22c2f0120157a0a90a3
hyunhoh <skins0329@gmail.com>
2023-02-21 09:11:56 +0900

    design: Container 컴포넌트 작성

--- client/stackoverflow/src/App.js
+++ client/stackoverflow/src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
 
 import Header from './components/Header';
 import Ask from './pages/Ask';
@@ -12,22 +13,31 @@ import Signup from './pages/Signup';
 import Tags from './pages/Tags';
 import Users from './pages/Users';
 
+const Container = styled.div`
+  max-width: 1264px;
+  width: 100%;
+  display: flex;
+  margin: 0 auto;
+`;
+
 const App = () => {
   return (
     <>
       <Header></Header>
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/questions" element={<Questions></Questions>}></Route>
-        <Route path="/tags" element={<Tags></Tags>}></Route>
-        <Route path="/users" element={<Users></Users>}></Route>
-        <Route path="/login" element={<Login></Login>}></Route>
-        <Route path="/signup" element={<Signup></Signup>}></Route>
-        <Route path="/ask" element={<Ask></Ask>}></Route>
-        <Route path="/edit" element={<Edit></Edit>}></Route>
-        <Route path="/mypage" element={<MyPage></MyPage>}></Route>
-        <Route path="/detail" element={<Detail></Detail>}></Route>
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/questions" element={<Questions></Questions>}></Route>
+          <Route path="/tags" element={<Tags></Tags>}></Route>
+          <Route path="/users" element={<Users></Users>}></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
+          <Route path="/signup" element={<Signup></Signup>}></Route>
+          <Route path="/ask" element={<Ask></Ask>}></Route>
+          <Route path="/edit" element={<Edit></Edit>}></Route>
+          <Route path="/mypage" element={<MyPage></MyPage>}></Route>
+          <Route path="/detail" element={<Detail></Detail>}></Route>
+        </Routes>
+      </Container>
     </>
   );
 };
